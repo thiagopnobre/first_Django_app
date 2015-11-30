0810d32455510e5c30f7c8f1712bb9e8891e4bc3
thiagopnobre <thiago_pnobre@hotmail.com>
2015-11-30 16:38:52 -0200

    Writing a detail view that actually do something and that's able to raise a 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from .models import Question
 
@@ -10,7 +10,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {}.".format(question_id))
+    try:
+        question = Question.objects.get(pk=question_id)
+        context = {'question': question}
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
