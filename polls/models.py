a90abf17e7ebd0ff850342bb26f823a2832ba97f
thiagopnobre <thiago_pnobre@hotmail.com>
2016-03-16 18:19:15 -0300

    Adding Python 2 unicode compatible

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,12 @@
+from __future__ import unicode_literals
 import datetime
 
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.utils import timezone
 
 
+@python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -19,6 +22,7 @@ class Question(models.Model):
     was_published_recently.short_description = 'Published recently?'
 
 
+@python_2_unicode_compatible
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
